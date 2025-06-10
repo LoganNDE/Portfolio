a8d4f61aa71f3e4531e192e902b31ebee41ed629
Logan <logannaranjo4@gmail.com>
2025-06-10 23:40:31 +0200

    Inicio del proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Titulo } from './titulo/titulo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Titulo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
   protected title = 'portfolio';
+  city = 'Barcelona'
 }
